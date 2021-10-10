5a2b4c44ed882945ba2087e52a8baf08892bc9e0
Charterino <charterinod@gmail.com>
2021-10-10 09:10:07 +0300

    Added support for env directories

--- ConsoleJS.Net/ConsoleJS.Net/JSCodeRunner.cs
+++ ConsoleJS.Net/ConsoleJS.Net/JSCodeRunner.cs
@@ -1,22 +1,26 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using Microsoft.VisualBasic;
 
 namespace ConsoleJS.Net
 {
     public class JSCodeRunner
     {
-        public JSCodeRunner(bool requirePackageManager = true)
+        public JSCodeRunner(string envPath = "", bool requirePackageManager = true)
         {
+            m_envPath = envPath;
             if (!"node".ExistsInPath())
                 throw new ApplicationException("Node isnt installed. Please double check your PATH.");
             if (!"npm".ExistsInPath() && !"yarn".ExistsInPath() && requirePackageManager)
                 throw new ApplicationException("Both NPM and YARN arent installed. Please double check your PATH.");
             if (!requirePackageManager) return;
             if ("yarn".ExistsInPath()) m_useYarn = true;
+            if (!Directory.Exists(m_envPath)) Directory.CreateDirectory(m_envPath);
         }
 
         private bool m_useYarn = false;
+        private string m_envPath = string.Empty;
 
         public string Evaluate(string script)
         {
@@ -27,6 +31,7 @@ namespace ConsoleJS.Net
             nodeProcess.StartInfo.Arguments = $" -e \"{script}\"";
             nodeProcess.StartInfo.UseShellExecute = false;
             nodeProcess.StartInfo.RedirectStandardOutput = true;
+            nodeProcess.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + m_envPath;
             nodeProcess.Start();
 
             var output = nodeProcess.StandardOutput.ReadToEnd();
@@ -42,6 +47,7 @@ namespace ConsoleJS.Net
             packageManagerProcess.StartInfo.UseShellExecute = false;
             packageManagerProcess.StartInfo.RedirectStandardOutput = true;
             packageManagerProcess.StartInfo.RedirectStandardError = true;
+            packageManagerProcess.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + m_envPath;
             packageManagerProcess.Start();
             var output = packageManagerProcess.StandardOutput.ReadToEnd();
             packageManagerProcess.WaitForExit();
